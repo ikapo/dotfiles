4127678af2f56d27dac636683196c85ea8b73bd3
ikapo <63411550+ikapo@users.noreply.github.com>
2023-03-20 21:27:37 +0200

    Added lsp to javascript and treemacs

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -43,7 +43,7 @@
        ophints                ; highlight the region an operation acts on
        (popup +all +defaults) ; tame sudden yet inevitable temporary windows
        ;;tabs                 ; a tab bar for Emacs
-       treemacs               ; a project drawer, like neotree but cooler
+       (treemacs +lsp)        ; a project drawer, like neotree but cooler
        ;;unicode              ; extended unicode support for various languages
        vc-gutter              ; vcs diff in the fringe
        vi-tilde-fringe        ; fringe tildes to mark beyond EOB
@@ -138,7 +138,7 @@
        ;;idris                ; a language you can depend on
        json                   ; At least it ain't XML
        ;;(java +meghanada)    ; the poster child for carpal tunnel syndrome
-       (javascript)           ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)      ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                ; a better, faster MATLAB
        ;;kotlin               ; a better, slicker Java(Script)
        ;;latex                ; writing papers in Emacs has never been so fun
